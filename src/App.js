37d3c00d894003fe0ff0a5827c34ae6f7c15c9e5
Amalia <cicala.amalia@gmail.com>
2022-05-25 17:54:44 +0200

    feat: add ex. Conditional Rendering 01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
+import { Age } from "./Age";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Hello />
-                <Welcome name={<strong>Amalia</strong>} age={26}/>
+                <Welcome name={<strong>Amalia</strong>}/>
             </div>
         )
     }
