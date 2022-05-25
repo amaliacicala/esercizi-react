84390b73dcf34a01aadc5f99fc0bc68782185839
Amalia <cicala.amalia@gmail.com>
2022-05-25 18:19:28 +0200

    feat: add ex. Conditional Rendering 04

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name={<strong>Amalia</strong>}/>
+                <Welcome name='Amalia' />
             </div>
         )
     }
--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
         return (
         <div>
             <p>Welcome, {this.props.name}!</p>
-            <Age age={26}/>
+            {this.props.name == 'John' && <Age age={26}/>}
          </div>
         )
     }
