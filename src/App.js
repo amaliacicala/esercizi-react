dfa4901e4bb828dbb3c173021ac4840015f2cb68
Amalia <cicala.amalia@gmail.com>
2022-05-27 20:27:52 +0200

    feat: add ex. Events 03

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
 import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
 
 export class App extends React.Component {
     render() {
@@ -10,6 +11,7 @@ export class App extends React.Component {
                 <Hello />
                 <Welcome name={<strong>Amalia</strong>} age={26}/>
                 <ClickCounter />
+                <ClickTracker />
             </div>
         )
     }
