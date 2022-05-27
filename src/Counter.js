80b2b1d186abdfca33d9bbed2430240e0fc41346
Amalia <cicala.amalia@gmail.com>
2022-05-27 18:26:36 +0200

    feat: add optional ex. Component Lifecycle 02

--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,4 @@
+import { clear } from "@testing-library/user-event/dist/clear";
 import React from "react"
 import { CounterDisplay } from "./CounterDisplay"
 
@@ -7,13 +8,19 @@ export class Counter extends React.Component {
     };
 
     componentDidMount() {
-        setInterval(() => {
+        this._interval = setInterval(() => {
             this.setState((state) => {
                 return {
                     count: state.count + (this.props.incrementAmount)
                 }
             })
         }, this.props.incrementInterval)
+    }
+
+    componentWillUnmount() {
+        if (this._interval) {
+            clearInterval(this._interval)
+        }
     }
 
     render() {
