17b8febdc2f0b1c2e032def8a9f1d8b05dfc936a
Amalia <cicala.amalia@gmail.com>
2022-06-08 17:32:26 +0200

    feat: add ex. Function Components 01

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Container } from "./Container";
 import { Welcome } from "./Welcome";
-import { Login } from "./Login";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Container title={<h1>My Container</h1>}>
-                    <Welcome />
-                    <Login />
+                    <Welcome name={<strong>Amalia</strong>}/>
                 </Container>
             </div>
         )
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export class Welcome extends React.Component {
-    render() {
-        return (
+// export class Welcome extends React.Component {
+//     render() {
+//         return (
+//         <div className="welcome">
+//             <p>Welcome, {this.props.name}!</p>
+//          </div>
+//         )
+//     }
+// }
+
+export function Welcome(props) {
+    return (
         <div className="welcome">
-            <p>Welcome!</p>
-         </div>
-        )
-    }
+            <p>Welcome, {props.name}!</p>
+        </div>
+    )
 }
