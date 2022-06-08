6273373360da9b38f00d7ed55b14253f778bc36c
Amalia <cicala.amalia@gmail.com>
2022-06-08 17:14:06 +0200

    feat: add ex. Context 02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,15 +1,6 @@
 import React from "react";
 import { LanguageContext } from "./LanguageContext";
 
-const Strings = {
-    en: {
-        WELCOME_MESSAGE: "Hello, world"
-    },
-    it: {
-        WELCOME_MESSAGE: "Ciao mondo"
-    }
-}
-
 export class DisplayLanguage extends React.Component {
     render() {
         return (
@@ -18,9 +9,9 @@ export class DisplayLanguage extends React.Component {
                 { language => {
                     return (
                         <div>
-                            <p>
-                                {Strings[language].WELCOME_MESSAGE}
-                            </p>
+                            <h1>
+                                {language}
+                            </h1>
                         </div>
                     )
                 }}
