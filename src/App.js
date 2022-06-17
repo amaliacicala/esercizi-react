ac2f296c959db7e0c06717fb8d3502a188c0143c
Amalia <cicala.amalia@gmail.com>
2022-06-17 20:42:39 +0200

    feat: add ex. React Router 04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
     return (
+    <>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/users:username">Github User</Link></li>
+            </ul>
+        </div>
+
         <Routes>
             <Route path="/" element={<Welcome name={<em>earthling</em>} />} />
             <Route path="/counter" element={<Counter />} />
             <Route path="users:username" element={<ShowGithubUser />} />
         </Routes>
+    </>
     )
 }
