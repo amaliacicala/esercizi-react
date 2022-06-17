a447aed9de0fa40cde606233178848e346552f44
Amalia <cicala.amalia@gmail.com>
2022-06-17 19:32:30 +0200

    feat: add ex. React Router 02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 import { Route, Routes } from "react-router-dom";
 
 export function App() {
     return (
         <Routes>
             <Route path="/" element={<Welcome name={<em>earthling</em>} />} />
+            <Route path="/counter" element={<Counter />} />
         </Routes>
     )
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export class Welcome extends React.Component {
     render() {
         return (
         <div>
             <h1>Welcome, {this.props.name}!</h1>
+            <Link to="/counter">Access counter</Link>
          </div>
         )
     }
