03288fd3151617fe287c8dd20070282b80b84445
Amalia <cicala.amalia@gmail.com>
2022-06-17 19:46:45 +0200

    feat: replace Link element with button element

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export class Welcome extends React.Component {
-    render() {
-        return (
-        <div>
-            <h1>Welcome, {this.props.name}!</h1>
-            <Link to="/counter">Access counter</Link>
-         </div>
-        )
+export function Welcome({ name }) {
+    const navigate = useNavigate();
+
+    function handleCounterBtnClick() {
+        navigate('/counter')
     }
+
+    return (
+        <div>
+            <h1>Welcome, {name}!</h1>
+            {/* <Link to="/counter">Access counter</Link> */}
+            <button 
+                style={{backgroundColor:'dodgerblue', padding:'0.5rem 1rem', border:'none', fontSize:'1rem', cursor:'pointer'}}
+                onClick={handleCounterBtnClick}
+            >Access counter</button>
+        </div>
+    )
 }
