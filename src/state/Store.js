9766331a4b26c40e45d616c500e67d0fee58b867
Amalia <cicala.amalia@gmail.com>
2022-06-18 13:17:39 +0200

    feat: add ex. Redux 03

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,13 +1,26 @@
 import { createStore } from "redux";
 import { counterReducer, incrementCount, resetCount } from "./CounterState";
+import { todosReducer, addTodo, removeTodo, editTodo } from "./TodosReducer";
 
-export const store = createStore(counterReducer)
+// export const store = createStore(counterReducer)
+
+// store.subscribe(() => {
+//     console.log(store.getState())
+// })
+
+// store.dispatch(resetCount())
+// store.dispatch(incrementCount(5))
+// store.dispatch(incrementCount(2))
+// store.dispatch(incrementCount(3))
+
+export const store = createStore(todosReducer)
 
 store.subscribe(() => {
     console.log(store.getState())
 })
 
-store.dispatch(resetCount())
-store.dispatch(incrementCount(5))
-store.dispatch(incrementCount(2))
-store.dispatch(incrementCount(3))
+store.dispatch(addTodo(1, 'Have breakfast', false))
+store.dispatch(addTodo(2, 'Study Redux', false))
+store.dispatch(addTodo(3, 'Go to sleep', false))
+store.dispatch(editTodo(1, 'Have breakfast', true))
+store.dispatch(removeTodo(1))
